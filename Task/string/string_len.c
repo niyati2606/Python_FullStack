db8376bf3ea38b9b377734ade0446bd5ae46c2e1
Niyati Patel <niyati2606>
2026-02-25 20:59:50 +0530

    string task

---
+++ Task/string/string_len.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main() {
+	
+	char name[50];
+	int i,length= 0;
+	
+	printf("Enter name : ");
+	gets(name);
+	
+	printf("Name %s ",name);
+
+	for(i=0; name[i] != '\0'; i++){
+		length++;
+	}
+	
+	printf("\nName Length = %d",length);
+		
+	getch();
+	
+}
