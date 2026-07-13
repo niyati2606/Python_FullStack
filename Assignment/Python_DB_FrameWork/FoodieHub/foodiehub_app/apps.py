6e153514960a949c3b80b04b7e896d5deb39f28c
Niyati Patel <niyati2606>
2026-07-13 16:54:37 +0530

    JavaScript and Django

---
+++ Assignment/Python_DB_FrameWork/FoodieHub/foodiehub_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FoodiehubAppConfig(AppConfig):
+    name = 'foodiehub_app'
---
+++ Javascript/first.js
@@ -0,0 +1,9 @@
+console.log("Hello this external file")
+console.log(10+20)
+
+// borwser
+
+// alert("pls feild  a data") //prompt alert window
+// confirm("Are your Sure ?") //prompt confirmation window
+// prompt("5+10 = ?")  //allowed input
+// print() //print window
