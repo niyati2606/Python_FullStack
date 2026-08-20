063a8421ca583be1bf8b84b5aa9722b23ec77ca7
Niyati Patel <niyati2606>
2026-08-20 20:41:43 +0530

    react - fundamentals session-1

---
+++ Assignment/React Fundamentals/Session 1/instaclonestarter/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TrendingSong from "./Component/TrendingSong";
+
+function App() {
+  return (
+
+    <div>
+      <h1>Welcome to My React Zomato App </h1>
+
+      <TrendingSong />
+    </div>
+
+  );
+}
+
+export default App;
