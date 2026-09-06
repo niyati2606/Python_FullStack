f999feedae4fd3024796e80f2e1308a4fb77ba00
Niyati Patel <niyati2606>
2026-09-06 20:15:39 +0530

    tailwind css

---
+++ Front-End/React JS/tailwindDemo/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import FontSizeComponeent from './Component/FontSizeComponeent'
+
+function App() {
+
+  return (
+    <>
+      <FontSizeComponeent />
+    </>
+  )
+}
+
+export default App
---
+++ Front-End/React JS/tailwindDemo/src/Component/FontSizeComponeent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function FontSizeComponeent() {
+    return (
+        <div>
+            <h1 className='text-xs'>Tailwind FontSize Demo</h1>
+            <h1 className='text-base'>Tailwind FontSize Demo</h1>
+            <h1 className='text-lg'>Tailwind FontSize Demo</h1>
+            <h1 className='text-xl'>Tailwind FontSize Demo</h1>
+            <h1 className='text-2xl'>Tailwind FontSize Demo</h1>
+            <h1 className='text-4xl'>Tailwind FontSize Demo</h1>
+        </div>
+
+    )
+}
+
+export default FontSizeComponeent
